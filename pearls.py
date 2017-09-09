1ecd4c9e41bb6e9e24d702af75de5b36c16dfd90
elevanth <elevanth@163.com>
2017-09-09 21:36:45 +0800

    resize methods

--- pearls.py
+++ pearls.py
@@ -4,7 +4,7 @@
 # If you wish to use CV2, you need to use the resize function.
 # For example, this will resize both axes by half:
 
-small = cv2.resize(image, (0,0), fx=0.5, fy=0.5) 
+small = cv2.resize(image, (0, 0), fx=0.5, fy=0.5)
 # and this will resize the image to have 100 cols (width) and 50 rows (height):
 
 resized_image = cv2.resize(image, (100, 50)) 
